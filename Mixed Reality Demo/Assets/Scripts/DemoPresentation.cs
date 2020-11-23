12836d1f4c856743215027420e27344055d88cdf
Matisse Faes <matttske@gmail.com>
2020-11-23 13:11:08 +0100

    Demo scene Complete

---
+++ Mixed Reality Demo/Assets/Scripts/DemoPresentation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoPresentation : MonoBehaviour
+{
+
+    [SerializeField]
+    private ParticleSystem[] smokers;
+
+    [SerializeField]
+    private AudioClip breathing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        smokers = new ParticleSystem[12];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Smoke"))
+        {
+            foreach (ParticleSystem smoke in smokers)
+            {
+                smoke.Play();
+            }
+        }
+        if (Input.GetButtonDown("Fast"))
+        {
+            Debug.Log(Input.mousePosition);
+        }
+    }
+}
