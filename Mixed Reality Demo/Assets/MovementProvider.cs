20503ccc8fc32ac2f600a6165af7f89596709126
Ajon <ajonsubedi@gmail.com>
2020-11-25 15:45:08 +0100

    slokdarm

---
+++ Mixed Reality Demo/Assets/MovementProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class MovementProvider : LocomotionProvider
+{
+    public List<XRController> controllers = null;
+
+    private CharacterController characterController = null;
+    private GameObject head = null;
+    protected override void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        head = GetComponent<XRRig>().cameraGameObject;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        PositionController();
+    }
+
+    private void PositionController()
+    {
+        // Get the head in local, playspace ground
+        float headHeight = Mathf.Clamp(head.transform.localPosition.y, 1, 2);
+        characterController.height = headHeight;
+
+        // cut in half, add skin
+        Vector3 newCenter = Vector3.zero;
+        newCenter.y = characterController.height / 2;
+        newCenter.y += characterController.skinWidth;
+
+        // let's move the character in local space as well
+        newCenter.x = head.transform.localPosition.x;
+        newCenter.z = head.transform.localPosition.z;
+
+        //Apply
+        characterController.center = newCenter;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
