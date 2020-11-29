6913dec122a02b79cf3cd71c3f313e9497540a7c
Matisse Faes <matttske@gmail.com>
2020-11-29 20:15:35 +0100

    Timer + Conversation added

---
+++ Mixed Reality Demo/Assets/Scripts/Lungs/DarkenRoom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkenRoom : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Smoke;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Smoke.GetComponent<SmokeTimer>().smokeHasEntered){
+            GetComponent<Light>().intensity = 0.3f;
+        }  
+    }
+}
